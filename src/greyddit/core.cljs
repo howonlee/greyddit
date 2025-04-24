67f218037a330ab162790f61ed4ea4a51a64216d
Howon Lee <hlee.howon@gmail.com>
2025-04-23 19:47:58 -0700

    dealing with it

--- src/greyddit/core.cljs
+++ src/greyddit/core.cljs
@@ -1,47 +1,59 @@
 (ns greyddit.core
   (:require
    [reagent.core :as r]
-   [reagent.dom :as rdom]))
+   ["react-dom/client" :as rdom-client]))
 
-(defonce state (r/atom {:url ""
-                        :html nil
-                        :dom  nil
+(defonce state (r/atom {:posts []
                         :error nil}))
 
-(defn fetch-html [url]
-  (-> (.fetch js/window url)
+(defn fetch-reddit-posts []
+  (-> (.fetch js/window "https://www.reddit.com/r/aww/top.json?limit=10")
       (.then (fn [res]
                (if (.-ok res)
-                 (.text res)
+                 (.json res)
                  (throw (js/Error. "Fetch failed")))))
-      (.then (fn [text]
-               (swap! state assoc
-                      :html text
-                      :dom (.parseFromString (js/DOMParser.) text "text/html")
-                      :error nil)))
+      (.then (fn [data]
+               (let [clj-data (js->clj data :keywordize-keys true)
+                     posts    (get-in clj-data [:data :children])]
+                 (print posts)
+                 (swap! state assoc
+                        :posts posts
+                        :error nil))))
       (.catch (fn [err]
                 (js/console.error err)
                 (swap! state assoc :error "Failed to load or CORS error")))))
 
+(defn post-table [posts]
+  [:table {:style {:border-collapse "collapse" :width "100%"}}
+   [:thead
+    [:tr
+     [:th {:style {:border "1px solid #ccc" :padding "8px"}} "Title"]
+     [:th {:style {:border "1px solid #ccc" :padding "8px"}} "Author"]
+     [:th {:style {:border "1px solid #ccc" :padding "8px"}} "Upvotes"]]]
+   [:tbody
+    (for [{:keys [data]} posts]
+      ^{:key (str (:id data))}
+      [:tr
+       [:td {:style {:border "1px solid #ccc" :padding "8px"}}
+        [:a {:href (str "https://reddit.com" (:permalink data))
+             :target "_blank"} (:title data)]]
+       [:td {:style {:border "1px solid #ccc" :padding "8px"}} (:author data)]
+       [:td {:style {:border "1px solid #ccc" :padding "8px"}} (:ups data)]])]])
+
 (defn app []
-  (let [{:keys [url html dom error]} @state]
+  (let [{:keys [posts error]} @state]
     [:div {:style {:padding "2rem"}}
-     [:h1 "ClojureScript URL DOM Loader"]
-     [:input {:type "text"
-              :value url
-              :on-change #(swap! state assoc :url (.. % -target -value))
-              :placeholder "Enter a URL (must allow CORS)"
-              :style {:width "400px" :margin-right "10px"}}]
-     [:button {:on-click #(fetch-html url)} "Load"]
+     [:h1 "Top r/aww Posts (old Reddit API)"]
+     [:button {:on-click fetch-reddit-posts
+               :style {:margin-bottom "1rem"}} "Load Posts"]
      (when error
        [:p {:style {:color "red"}} error])
-     (when dom
-       [:div {:style {:margin-top "2rem"}}
-        [:h2 "DOM Preview:"]
-        [:pre (subs (.-outerHTML (.-documentElement dom)) 0 1000) "..."]])]))
+     (when (seq posts)
+       [post-table posts])]))
 
 (defn ^:dev/after-load start []
-  (rdom/render [app] (.getElementById js/document "app")))
+  (let [root (rdom-client/createRoot (.getElementById js/document "app"))]
+    (.render root (r/as-element [app]))))
 
 (defn ^:export init []
   (start))
